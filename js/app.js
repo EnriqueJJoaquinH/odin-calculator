01abc822dbeaff65b3e7320c3896a14c84798423
enriquejh <enriquejjoaquinh@gmail.com>
2025-01-06 14:15:33 -0600

    Allow the calculator to make calculations
    Apart from the Ans button, every other button does what it should do.
    Some special cases, such calling the operate function with just one
    argument, pressing a button (different from equal) exactly after an
    operation has been calculated, and the functionality of buttons such as
    'AC', 'DEL' and the decimal point buttons, are already integrated into
    the page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 // ? Global variables
-let displayContent = '';
 let operator1 = 0;
 let operator2 = 0;
 let operation = '';
 let lastAnswer = 0;
+
+let opTxt = document.querySelector('#operation');
+let resTxt = document.querySelector('#result');
+let buttons = document.querySelector('#buttons');
+let equalBtn = document.querySelector('#equal-btn');
 
 function add(a, b) {
     return a + b;
@@ -19,41 +23,62 @@ function multiply (a, b) {
 
 function divide (a, b) {
     if (b == 0) {
-        return 'Division by 0 not allowed'
+        return 'Division by 0 not allowed';
     }
     return a / b;
 }
 
-function operate(a, operator, b) {
+function operate(a, operator = '', b = 0) {
+    if (opTxt.textContent == '')
+        return '';
+
     let result = 0;
     switch(operator){
         case '+':   result = add(a, b);         break;
-        case '-':   result = subtract(a, b);    break;
-        case '*':   result = multiply(a, b);    break;
-        case '/':   result = divide(a, b);      break;
+        case '−':   result = subtract(a, b);    break;
+        case '×':   result = multiply(a, b);    break;
+        case '÷':   result = divide(a, b);      break;
+        case '':    result = a;                 break;
     }
-    console.log(result);
 
     return result;
 }
 
-let opTxt = document.querySelector('#operation');
-let buttons = document.querySelector('#buttons');
-buttons.addEventListener('click', (event) => {
+function getOperationComponents(operationStr) {
+    let operationArr = operationStr.split(' ');
+    operator1 = +operationArr[0];
+    operation = operationArr[1];
+    operator2 = +operationArr[2];
+}
+
+function clearScreen() {
+    opTxt.textContent = '';
+    resTxt.textContent = '';
+}
+
+function populateScreen(event) {
     if (event.target.id === 'buttons' || Array.from(event.target.classList).includes('row'))
         return;
+
+    if (event.target === equalBtn){
+        getOperationComponents(opTxt.textContent);
+        resTxt.textContent = `${operate(operator1, operation, operator2)}`;
+        return;
+    }
+
+    if (resTxt.textContent != '')
+        clearScreen();
     switch (event.target.textContent) {
         case 'AC':
-            opTxt.textContent = '';
+            clearScreen();
             break;
         case 'DEL':
-            opTxt.textContent = opTxt.textContent.trimEnd().slice(0, -1)
+            opTxt.textContent = opTxt.textContent.trimEnd().slice(0, -1);
             break;
         case '+':
-        case '-':
+        case '−':
         case '×':
         case '÷':
-        case '=':
             opTxt.textContent += ` ${event.target.textContent} `;
             break;
         case '•':
@@ -63,4 +88,6 @@ buttons.addEventListener('click', (event) => {
             opTxt.textContent += event.target.textContent;
             break;
     }
-})
+}
+
+buttons.addEventListener('click', populateScreen);
